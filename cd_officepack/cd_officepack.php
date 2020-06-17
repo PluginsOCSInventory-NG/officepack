adf095c434679f0acfaa15adc91e75ee9a87a79c
Charlene Auger <root@OCS-srv-trunk2-ocs.factorfx.com>
2020-06-17 13:19:54 +0000

    Fix mistake

--- cd_officepack/cd_officepack.php
+++ cd_officepack/cd_officepack.php
@@ -9,41 +9,52 @@
 // Please refer to the General Public Licence http://www.gnu.org/ or Licence.txt
 //====================================================================================
  
-	if(AJAX){
-		parse_str($protectedPost['ocs']['0'], $params);
-		$protectedPost+=$params;
-		ob_start();
-		$ajax = true;
-	}
-	else{
-		$ajax=false;
-	}
-	print_item_header($l->g(23004));
-	if (!isset($protectedPost['SHOW']))
-		$protectedPost['SHOW'] = 'NOSHOW';
-	$form_name="officepack";
-	$table_name=$form_name;
-	$tab_options=$protectedPost;
-	$tab_options['form_name']=$form_name;
-	$tab_options['table_name']=$table_name;
-	echo open_form($form_name);
-	$list_fields=array( $l-g(23005) => 'PRODUCT',
-	$l->g(23006) => 'OFFICEVERSION',
-	$l->g(23007) => 'TYPE',
-	$l->g(23008) => 'OFFICEKEY',
-	);
-	$list_col_cant_del=$list_fields;
-	$default_fields= $list_fields;
-	$sql=prepare_sql_tab($list_fields);
-	$sql['SQL']  .= "FROM officepack WHERE (hardware_id = $systemid)";
-	array_push($sql['ARG'],$systemid);
-	$tab_options['ARG_SQL']=$sql['ARG'];
-	$tab_options['ARG_SQL_COUNT']=$systemid;
-	ajaxtab_entete_fixe($list_fields,$default_fields,$tab_options,$list_col_cant_del);
-	echo close_form();
-	if ($ajax){
-		ob_end_clean();
-		tab_req($list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$tab_options);
-		ob_start();
-	}
+if(AJAX) {
+	parse_str($protectedPost['ocs']['0'], $params);
+	$protectedPost+=$params;
+	ob_start();
+	$ajax = true;
+} else {
+	$ajax=false;
+}
+
+print_item_header($l->g(23004));
+
+if (!isset($protectedPost['SHOW'])) {
+	$protectedPost['SHOW'] = 'NOSHOW';
+}
+
+$form_name="officepack";
+$table_name=$form_name;
+$tab_options=$protectedPost;
+$tab_options['form_name']=$form_name;
+$tab_options['table_name']=$table_name;
+
+echo open_form($form_name);
+
+$list_fields=array( $l->g(23005) => 'PRODUCT',
+$l->g(23006) => 'OFFICEVERSION',
+$l->g(23007) => 'TYPE',
+$l->g(23008) => 'OFFICEKEY',
+);
+
+$list_col_cant_del=$list_fields;
+$default_fields= $list_fields;
+
+$sql=prepare_sql_tab($list_fields);
+$sql['SQL']  .= "FROM officepack WHERE (hardware_id = $systemid)";
+array_push($sql['ARG'],$systemid);
+
+$tab_options['ARG_SQL']=$sql['ARG'];
+$tab_options['ARG_SQL_COUNT']=$systemid;
+
+ajaxtab_entete_fixe($list_fields,$default_fields,$tab_options,$list_col_cant_del);
+
+echo close_form();
+
+if ($ajax) {
+	ob_end_clean();
+	tab_req($list_fields,$default_fields,$list_col_cant_del,$sql['SQL'],$tab_options);
+	ob_start();
+}
 ?>
